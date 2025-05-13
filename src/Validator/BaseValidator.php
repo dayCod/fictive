2c9bc0d45f3f86567f28e032810f4420aae79a76
dayCod <alayarahman07@gmail.com>
2025-05-13 18:08:34 +0700

    adds data result contracts and base validator

---
+++ src/Validator/BaseValidator.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daycode\Fictive\Validator;
+
+class BaseValidator
+{
+    //
+}
---
+++ src/Validator/DataResultValidator.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daycode\Fictive\Validator;
+
+class DataResultValidator extends BaseValidator
+{
+    /**
+     * check is valid fullName method
+     */
+    public function checkIsValidFullName(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid phoneNumber method
+     */
+    public function checkIsValidPhoneNumber(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid dateOfBirth method
+     */
+    public function checkIsValidDateOfBirth(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid maritalStatus method
+     */
+    public function checkIsValidMaritalStatus(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid religion method
+     */
+    public function checkIsValidReligion(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid hobby method
+     */
+    public function checkIsValidHobby(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid bloodGroup method
+     */
+    public function checkIsValidBloodGroup(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid latitude method
+     */
+    public function checkIsValidLatitude(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid longitude method
+     */
+    public function checkIsValidLongitude(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid country method
+     */
+    public function checkIsValidCountry(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid state method
+     */
+    public function checkIsValidState(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid city method
+     */
+    public function checkIsValidCity(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid streetName method
+     */
+    public function checkIsValidStreetName(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid address method
+     */
+    public function checkIsValidAddress(): bool
+    {
+        return true;
+    }
+
+    /**
+     * check is valid postalCode method
+     */
+    public function checkIsValidPostalCode(): bool
+    {
+        return true;
+    }
+}
